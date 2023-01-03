c6a2aacf829c133d7dc66bda312cf69797c66f6b
LEE JIN YOUNG <jinijnleelee@gmail.com>
2023-01-03 12:15:01 +0900

    네트워크 클라이언트 연습

---
+++ client/src/edu/kh/network/run/ClientRun.java
@@ -0,0 +1,14 @@
+package edu.kh.network.run;
+
+import edu.kh.network.model.service.TCPClient;
+
+public class ClientRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TCPClient client = new TCPClient();
+		client.ClientStart();
+	}
+
+}
